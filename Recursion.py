00760ba8743e0c6e01c40ed0c2b4d0c911147982
narevayin <babayanare@gmail.com>
2022-09-25 19:51:22 +0400

    Recursion

--- Recursion.py
+++ Recursion.py
@@ -1,10 +1,23 @@
 # ex 173
-def summ():
+# def summ():
 
-    x = input("Enter a number (blank to quit): ")
-    if x == "":
-        return 0
-    else:
-        return float(x) + summ()
+#     x = input("Enter a number (blank to quit): ")
+#     if x == "":
+#         return 0
+#     else:
+#         return float(x) + summ()
 
-print(summ())
+# print(summ())
+
+# ex 174
+
+# def gcd(x , y):
+#     if y == 0:
+#         return x
+#     else:
+#         return gcd(y, x % y)
+
+# print(gcd(65,15))
+
+# ex 175
+

ad15a60ed4c10e99c8d481f7afbc167188cd3970
narevayin <babayanare@gmail.com>
2022-09-26 15:26:31 +0400

    recursion

--- Recursion.py
+++ Recursion.py
@@ -21,3 +21,54 @@
 
 # ex 175
 
+# def Binary(n):
+#    if n > 1:
+#        Binary(n//2)
+#    print (n % 2,end = '')
+
+# print(Binary(34))
+
+# ex 178 
+
+# def isPalindrome(s):
+#     if len(s) <= 1:
+#         return True
+#     else:
+#         return s[0] == s[len(s) - 1] and isPalindrome(s[1 : len(s) - 1])
+# def main():
+
+#     line = input("Введите строку: ")
+
+#     if isPalindrome(line):
+#         print("Palindrome")
+#     else:
+#         print("Not Palindrome")
+
+# main()
+
+# ex 180
+
+# def distance(s, t):
+
+#     if len(s) == 0:
+#         return len(t)
+#     elif len(t) == 0:
+#         return len(s)
+#     else:
+#         cost = 0
+#     if s[len(s) - 1] != t[len(t) - 1]:
+#         cost = 1
+
+#     d1 = distance(s[0 : len(s) - 1], t) + 1
+#     d2 = distance(s, t[0 : len(t) - 1]) + 1
+#     d3 = distance(s[0 : len(s) - 1] , t[0 : len(t) - 1]) +  cost
+
+#     return min(d1, d2, d3)
+
+# def main():
+
+#  s1 = input("enter string: ")
+#  s2 = input("enter string: ")
+
+#  print( distance(s1, s2))
+# main()
